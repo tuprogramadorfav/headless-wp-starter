c30700842b28983c185339a67ff60371adcfb305
Juan Antonio Romero <juan.romero@code.berlin>
2024-04-09 04:17:52 -0500

    added repo

--- frontend-graphql/src/config.js
+++ frontend-graphql/src/config.js
@@ -3,7 +3,7 @@ let url = 'http://localhost:8080/graphql';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
-  url = 'http://wp-headless:8080/graphql';
+  url = 'https://techmanager.io/graphql';
 }
 const Config = {
   gqlUrl: url,
